d58a2b7e330eae963c82fff86f0d4b010f3370db
Chutipon Pongpanit <chutiponpon@7eleven.cp.co.th>
2017-03-10 11:55:13 +0700

    loop heroes

--- src/app/gosoft.component.ts
+++ src/app/gosoft.component.ts
@@ -1,15 +1,33 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
 
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
     selector: 'app-gosoft',
     //template : '<h1>{{title}}</h1><h1>{{hero}}</h1>'
     //template : '<h1> {{hero.id}} {{hero.title}}</h1>'
     templateUrl : './gosoft.component.html'
+    //template : '<h1> {{hero[0]}} </h1>'
+    
 })
 export class GosoftComponent {
   title = 'gosoft works!!!!!!!!';
   //hero = 'Wonder Women';
-  hero: Hero = {id: 1, name: 'wonderwomen'};
-  //hero: Hero = new Hero(1, 'WonderMan');
+  heroes = HEROES;
+  hero: Hero = new Hero(1, 'WonderMan');
+
 }
+
+
