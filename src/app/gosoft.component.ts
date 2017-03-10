8fc8acf8c02d1dc2f74b009c615a4766e7e64218
Chutipon Pongpanit <chutiponpon@7eleven.cp.co.th>
2017-03-10 11:09:48 +0700

    coding constructor style

--- src/app/gosoft.component.ts
+++ src/app/gosoft.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 
-@Component({//annotation , rectorator
+@Component({
     selector: 'gosoft-root',
-    template : '<h1>{{title}}<h1>'
+    //template : '<h1>{{title}}</h1><h1>{{hero}}</h1>'
+    template : '<h1> {{hero.id}} {{hero.title}}</h1>'
 })
 export class GosoftComponent {
   title = 'gosoft works!!!!!!!!';
+  //hero = 'Wonder Women';
+  hero: Hero = {id: 1, title: 'wonderwomen'};
+  //hero: Hero = new Hero(1, 'WonderMan');
 }
 
-
-
+class Hero{
+    constructor(id: number, public title: string) {}
+    //constructor(private id: number, private title: string) {}
+}
