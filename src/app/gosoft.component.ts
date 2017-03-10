b857e695cbe047609c7c693e1dd87731a7057f0b
Chutipon Pongpanit <chutiponpon@7eleven.cp.co.th>
2017-03-10 11:17:04 +0700

    update3

--- src/app/gosoft.component.ts
+++ src/app/gosoft.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
-    selector: 'gosoft-root',
+    selector: 'app-gosoft',
     //template : '<h1>{{title}}</h1><h1>{{hero}}</h1>'
     template : '<h1> {{hero.id}} {{hero.title}}</h1>'
 })
@@ -10,9 +11,4 @@ export class GosoftComponent {
   //hero = 'Wonder Women';
   hero: Hero = {id: 1, title: 'wonderwomen'};
   //hero: Hero = new Hero(1, 'WonderMan');
-}
-
-class Hero{
-    constructor(id: number, public title: string) {}
-    //constructor(private id: number, private title: string) {}
 }
---
+++ src/app/hero.ts
@@ -0,0 +1,4 @@
+export class Hero{
+    constructor(public id: number, public title: string) {}
+    //constructor(private id: number, private title: string) {}
+}
