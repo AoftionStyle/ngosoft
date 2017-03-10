79a8897506f6075838d3106f658c03f38e7c63e7
Chutipon Pongpanit <chutiponpon@7eleven.cp.co.th>
2017-03-10 10:53:12 +0700

    continue2

---
+++ src/app/gosoft.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({//annotation , rectorator
+    selector: 'gosoft-root',
+    template : '<h1>{{title}}<h1>'
+})
+export class GosoftComponent {
+  title = 'gosoft works!!!!!!!!';
+}
+
+
+
